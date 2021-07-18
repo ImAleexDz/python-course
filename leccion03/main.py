25efc790f022045a45b0ad270a16d471c39d5e63
ImAleexDz <alexndro7145@gmail.com>
2021-07-17 21:41:04 -0500

    sentencias de control terminadas

--- leccion03/main.py
+++ leccion03/main.py
@@ -17,18 +17,71 @@
 #     print('Condición no reconocida')
 
 # Ejercicio - Conversión de Número a Texto
-numero = int(input('Escribe un valor entre 1 y 3: '))
-numeroTexto = ''
+# numero = int(input('Escribe un valor entre 1 y 3: '))
+# numeroTexto = ''
 
-if numero == 1:
-    numeroTexto = 'Número uno'
-elif numero == 2:
-    numeroTexto = "Número dos"
-elif numero == 3:
-    numeroTexto = "Número tres"
-else: 
-    numeroTexto = "Valor fuera de rango"
+# if numero == 1:
+#     numeroTexto = 'Número uno'
+# elif numero == 2:
+#     numeroTexto = "Número dos"
+# elif numero == 3:
+#     numeroTexto = "Número tres"
+# else: 
+#     numeroTexto = "Valor fuera de rango"
 
-print(f'Número proporciondo: {numero}: {numeroTexto}')
+# print(f'Número proporciondo: {numero}: {numeroTexto}')
 
 
+# Sintaxis if else simplificada (Operador terniario)
+# condicion = False
+
+# print('Condicion verdadera') if condicion else print('Condicion falsa')
+
+# Ejercicio - Calcular la estación según el mes proporcionado
+# mes = int(input('Proporciona mes del año (1-12): '))
+# estacion = None
+
+# if mes == 1 or mes == 2 or mes == 12:
+#     estacion = 'Invierno'
+# elif mes == 3 or mes == 4 or mes == 5:
+#     estacion = 'Primavera'
+# elif mes == 6 or mes == 7 or mes == 8:
+#     estacion = 'Verano'
+# elif mes == 9 or mes == 10 or mes == 11:
+#     estacion = 'Otoño'
+# else:
+#     estacion = 'Mes incorrecto'
+
+
+# print(f'Para el mes {mes} la estación es: {estacion}')
+
+# Ejercicio - Estapas de vida según edad
+# edad = int(input('Proporciona tu edad: '))
+# etapa = None
+
+# if 0 <= edad < 10:
+#     etapa = 'La infancia es increíble'
+# elif 10 <= edad < 20:
+#     etapa = 'Muchos cambios y mucho estudio'
+# elif 20 <= edad < 30:
+#     etapa = 'Amor y comienza el trabajo'
+# else: 
+#     etapa = 'Etapa de vida no reconocida'
+
+# print(f'{edad} corresponde a la siguiente etapa de vida: {etapa}')
+
+# Ejercicio - Sistema de calificaciones
+calificacion = float(input('Proporciona un número entre 0 y 10: '))
+
+if 9 <= calificacion <= 10:
+    print('A')
+elif 8 <= calificacion < 9:
+    print('B')
+elif 7 <= calificacion < 8:
+    print('C')
+elif 6 <= calificacion < 7:
+    print('D')
+elif 0 <= calificacion < 6:
+    print('F')
+else:
+    print('Valor desconocido')
---
+++ leccion04-env/main.py
@@ -0,0 +1,61 @@
+# Ciclo While en Python
+# condicion = True
+
+# while condicion:
+# loop infinito
+#     print('Ejecutando ciclo while')
+# else:
+#     print('Fin del ciclo while')
+
+# contador = 0
+
+# while contador < 3:
+#     print(contador)
+#     contador += 1 #Incrementando + 1
+# else:
+#     print('Fin del ciclo while')
+
+# Tarea - Imprimir los números naturales del 0 al 10 utilizando el ciclo while
+# contador = 0
+
+# while contador <= 10:
+#     print(contador)
+#     contador += 1
+# else:
+#     print('Fin del ciclo while')
+
+# Tarea - Imprimir números del 5 al 1 de manera descendente
+# i = 5
+
+# while i != 0:
+#     print(i)
+#     i -= 1
+
+# Ciclo for en Python
+# cadena = 'Hola'
+
+# for letra in cadena:
+#     print(letra)
+# else:
+#     print('Fin ciclo for')
+
+# Palabra break en python
+
+# for letra in 'Holanda':
+#     if letra == 'a':
+#         print(f'Letra encontrada: {letra}')
+#     # break rompe con nuestro ciclo, no imprime el nisiquiera el else
+#         break
+# else:
+#     print('Fin ciclo for')
+
+# Palabra continue en Python
+# for i in range(6):
+#     if i % 2 == 0:
+#         print(f'Valor: {i}')
+
+# Continue ejecuta la siguiente itereación y ya no ejecuta ninguna de las líneas siguientes
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f'Valor: {i}')
